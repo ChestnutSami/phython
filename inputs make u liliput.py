86d3c6e30b0c4d214d4972cb27ef9a4a5b2e4b67
ChestnutSami <samimashfi003@gmail.com>
2024-08-29 00:11:04 +0600

    finally

---
+++ inputs make u liliput.py
@@ -0,0 +1,13 @@
+name= input("Enter your name")
+age= input("Your age")
+print("Hello "+name+" Your age is "+age+"!" )
+
+num1= input("Number one ")
+num2= input("Number two ")
+num3= int(num1) + int(num2) 
+print (num3)
+
+num1= input("Number one ")
+num2= input("Number two ")
+num3= float(num1) + float(num2) 
+print (num3)
